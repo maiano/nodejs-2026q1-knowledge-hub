e941bd68805f9cfb3dfcc281152d6a38d30e69de
maiano <maiano@yandex.ru>
2026-04-05 21:25:29 +0200

    feat: add filtration and pagination to the article module

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -6,23 +6,61 @@ import {
   Param,
   ParseUUIDPipe,
   Post,
+  Put,
+  Query,
   HttpCode,
   HttpStatus,
 } from '@nestjs/common';
+
 import { ArticleService } from './article.service';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+import { FilterArticleDto } from './dto/filter-article.dto';
 
 @Controller('article')
 export class ArticleController {
-  constructor(private service: ArticleService) {}
+  constructor(private readonly service: ArticleService) {}
 
-  @Post()
-  create(@Body() dto: any) {
-    return this.service.create(dto);
+  @Get()
+  findAll(@Query() query: FilterArticleDto) {
+    const hasFilters =
+      query.status !== undefined ||
+      query.categoryId !== undefined ||
+      query.tag !== undefined;
+
+    const hasPagination =
+      query.page !== undefined ||
+      query.limit !== undefined ||
+      query.sortBy !== undefined ||
+      query.order !== undefined;
+
+    if (hasFilters && !hasPagination) {
+      return this.service.findFiltered(query).data;
+    }
+
+    if (hasFilters || hasPagination) {
+      return this.service.findFiltered(query);
+    }
+
+    return this.service.findAll();
   }
 
   @Get(':id')
   findById(@Param('id', new ParseUUIDPipe()) id: string) {
     return this.service.findById(id);
+  }
+
+  @Post()
+  create(@Body() dto: CreateArticleDto) {
+    return this.service.create(dto);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() dto: UpdateArticleDto,
+  ) {
+    return this.service.update(id, dto);
   }
 
   @Delete(':id')
