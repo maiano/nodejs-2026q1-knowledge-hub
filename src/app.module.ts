3340411124f285508855e586020ed95cf6c8b312
maiano <maiano@yandex.ru>
2026-04-11 20:40:21 +0200

    chore: add health endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { UserModule } from 'src/user/user.module';
 import { ArticleModule } from 'src/article/article.module';
 import { CommentModule } from 'src/comment/comment.module';
 import { CategoryModule } from 'src/category/category.module';
+import { AppController } from 'src/app.controller';
 
 @Module({
   imports: [
@@ -13,5 +14,7 @@ import { CategoryModule } from 'src/category/category.module';
     CommentModule,
     CategoryModule,
   ],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
