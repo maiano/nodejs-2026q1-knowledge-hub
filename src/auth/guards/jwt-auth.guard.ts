bb4a0251797c40fcb7f219c1ba3c849e5caf0914
maiano <maiano@yandex.ru>
2026-04-19 16:54:01 +0200

    feat: add roles guard and decorators

---
+++ src/auth/guards/jwt-auth.guard.ts
@@ -0,0 +1,43 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+import { JwtPayload } from '../strategies/jwt.strategy';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) return true;
+
+    return super.canActivate(context);
+  }
+
+  handleRequest<TUser = JwtPayload>(
+    err: unknown,
+    user: unknown,
+    info: unknown,
+    _context: ExecutionContext,
+    _status?: unknown,
+  ): TUser {
+    if (err || !user) {
+      const message =
+        info instanceof Error ? info.message : 'Invalid or expired token';
+
+      throw new UnauthorizedException(message);
+    }
+
+    return user as TUser;
+  }
+}
